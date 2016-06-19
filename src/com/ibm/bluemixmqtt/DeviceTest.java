41a69b0e61623babea2ce92952e10fdc8e238cfd
karemre <yigitogluemre@gmail.com>
2016-06-19 15:51:51 -0700

    fix errors

--- src/com/ibm/bluemixmqtt/DeviceTest.java
+++ src/com/ibm/bluemixmqtt/DeviceTest.java
@@ -110,8 +110,8 @@ public class DeviceTest {
 		}
 
 
-		ExecuteShellComand obj = new ExecuteShellComand();
-		String output = obj.executeCommand("docker stop $(docker ps");
+
+		String output = executeCommand("docker stop $(docker ps");
 
 		System.out.println(output);
 
